5f720585a70e3d55eacd5e5ea693ef6ea8b06482
ntorguud03 <jenny.oyuka@gmail.com>
2021-03-26 21:58:45 +0000

    Add contents

---
+++ map.php
@@ -0,0 +1,46 @@
+<?php include '../includes/big-header.php' ?>
+
+<body>
+    <div id="wrapper">
+        <div class="section">
+            <h2><?= $PageID ?></h2>
+            <p>Iframe a responsive Google Map to the campus.</p>
+            <div class="mapouter">
+                <div class="gmap_canvas">
+                    <iframe width="100%" height="400" id="gmap_canvas" src="https://maps.google.com/maps?q=1701%20broadway%20seattle&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+                    </iframe>
+                    <a href="https://putlocker-is.org">putlocker</a>
+                    <br>
+                    <style>
+                        .mapouter {
+                            position: relative;
+                            text-align: right;
+                            height: 400px;
+                            width: 100%;
+                        }
+                    </style>
+                    <a href="https://www.embedgooglemap.net">google maps for my website</a>
+                    <style>
+                        .gmap_canvas {
+                            overflow: hidden;
+                            background: none !important;
+                            height: 400px;
+                            width: 100%;
+                        }
+                    </style>
+                </div>
+            </div>
+        </div>
+
+        <div class="aside">
+            <h3>Contact info</h3>
+            <p>Seattle Central College - Science and Math Building</p>
+            <p>1701 Broadway, Seattle, WA 98122</p>
+            <p><a href="tel:12069343800">Phone: (206) 934-3800</a></p>
+        </div>
+    </div>
+
+</body>
+
+</html>
+<?php include '../includes/big-footer.php' ?>
