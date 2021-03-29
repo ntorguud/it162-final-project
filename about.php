b6a4aa9e9c625a1ee88945135e5664a775cd8bf7
Naran Torguud <ntorguud@gmail.com>
2021-03-29 05:19:17 +0000

    Edit text

--- about.php
+++ about.php
@@ -1,33 +1,60 @@
 <?php include 'includes/fp-header.php' ?>
 
-<main>
+<body>
+    <div id="wrapper">
 
-    <h1> About "Enerel Dent" Dental Clinic</h1>
-    <figure>
-        <img src="images/enerel-family.jpeg" alt="group">
-        <figcaption>"Enerel Dent" Dental Clinic Doctors</figcaption>
-    </figure>
-    <p> When you come to our dental practice for the first time, we think you’ll be amazed. Not just because of our unique dental facility in the heart of "Buren Dent" with state-of-the-art equipment. Not just because our patients are seen right away and have virtually no wait time. It’s because of our doctors and our exceptional team who are committed to providing comfortable, gentle dentistry in an atmosphere that will make you feel pampered.</p>
+        <main>
 
-    <p><b>Please enjoy our comfort amenities while we provide you with a smile that will last a lifetime:</b>
-    </p>
+            <div class="row">
 
-    <ul>
+                <div class="text">
 
-        <li><i>~ Nitrous oxide is available if needed</i></li>
-        <br>
-        <li><i>~ Headphones are available if you want to listen to any music or podcasts</i></li>
-        <br>
-        <li><i>~ Sunglasses are available if you have light sensitivity</i></li>
-        <br>
-        <li><i>~ Blankets available if you tend to get chilly</i></li>
-        <br>
-        <li><i>~ Comfortable chairs in every room to help you relax at your appointment</i></li>
-    </ul>
-</main>
+                    <h1> About "Enerel Dent" Dental Clinic</h1>
+                    <figure>
+                        <img src="images/enerel-family.jpeg" alt="group">
+                        <figcaption><i>"Enerel Dent" Dental Clinic Doctors</i></figcaption>
+                    </figure>
+                    <br>
+                    <br>
+                    <p> When you come to our dental practice for the first time, we think you’ll be amazed. Not just because of our unique dental facility in the heart of "Enerel Dent" with state-of-the-art equipment. Not just because our patients are seen right away and have virtually no wait time. It’s because of our doctors and our exceptional team who are committed to providing comfortable, gentle dentistry in an atmosphere that will make you feel pampered.</p>
 
-<?php include 'includes/fp-footer.php' ?>
-</div> <!-- end wrapper -->
+                    <p><b>Please enjoy our comfort amenities while we provide you with a smile that will last a lifetime:</b>
+                    </p>
+
+                    <ul>
+
+                        <li><i>~ Nitrous oxide is available if needed</i></li>
+                        <br>
+                        <li><i>~ Headphones are available if you want to listen to any music or podcasts</i></li>
+                        <br>
+                        <li><i>~ Sunglasses are available if you have light sensitivity</i></li>
+                        <br>
+                        <li><i>~ Blankets available if you tend to get chilly</i></li>
+                        <br>
+                        <li><i>~ Comfortable chairs in every room to help you relax at your appointment</i></li>
+                    </ul>
+                </div>
+                <!--end text-->
+
+            </div>
+            <!--end row-->
+
+
+            <div class="row">
+
+                <div class="text">
+                    <iframe src="https://www.facebook.com/plugins/video.php?height=420&href=https%3A%2F%2Fwww.facebook.com%2FEnereldent%2Fvideos%2F1800019393635654%2F&show_text=false&width=560" width="560" height="420" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true">
+                    </iframe>
+                </div>
+                <!--end text-->
+
+            </div>
+            <!--end row-->
+
+        </main>
+
+        <?php include 'includes/fp-footer.php' ?>
+    </div> <!-- end wrapper -->
 </body>
 
 </html>
