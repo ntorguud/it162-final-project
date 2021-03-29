733d4a24244e12c53c2703677e0f8ec5625c742b
Naran Torguud <ntorguud@gmail.com>
2021-03-29 01:27:43 +0000

    Edit text

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <!--start slideshow-->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
-<script src="js/jquery.blueberry.js"></script>
+<script src="js/jquery.blueberry.js"></script> 
 
 <script>
     $(window).load(function() {
@@ -10,71 +10,65 @@
     });
 </script>
 
-    <div class="blueberry">
-        <ul class="slides">
-            <li><img src="images/enerel-front.jpeg" alt="image"></li>
-            <li><img src="images/enerel-family.jpeg" alt="images"></li>
-            <li><img src="images/enerel-conference.jpeg" alt="images"></li>
-        </ul>
+<div class="blueberry">
+    <ul class="slides">
+        <li><img src="images/enerel-front.jpeg" alt="image"></li>
+        <li><img src="images/enerel-family.jpeg" alt="images"></li>
+        <li><img src="images/enerel-conference.jpeg" alt="images"></li>
+    </ul>
+</div>
+<!-- end slideshow -->
+<br>
+<main>
+    <h1>Smile With Confidence</h1>
+    <div class="row">
+
+        <div class="text">
+            <h2> Welcome </h2>
+            <img class="left" src="images/adult1.jpg" alt="adult">
+            <p>Welcome to "Enerel Dent" Dental Clinic in Ulaanbaatar!
+                Our team strives to We provide multi functional dental service to each and every client.<br>
+                We always make sure that our clients are satisfied and happy every time they walk through our doors. Whether it’s a checkup or in need of corrective care, you can trust in us.<br>
+                Our goal is to make you shine your biggest and brightest smile!</p>
+
+        </div>
+        <!--end text-->
+
     </div>
-    <!-- end slideshow -->
-    <br>
-    <main>
-        <h1>Smile With Confidence</h1>
-        <div class="row">
+    <!--end row-->
 
-            <div class="text">
-                <h2> Welcome </h2>
-                <img class="left" src="images/adult1.jpg" alt="adult">
-                <p>Welcome to "Enerel Dent" Dental Clinic in Ulaanbaatar!
-                    Our team strives to provide the best service to each and every client.<br>
-                    We always make sure that our clients are satisfied and happy every time they walk through our doors. Whether it’s a checkup or in need of corrective care, you can trust in us.<br>
-                    Our goal is to make you shine your biggest and brightest smile!</p>
+    <div class="row">
 
-            </div>
-            <!--end text-->
-
+        <div class="text">
+            <h2> Pediatric Dental At "Enerel Dent" Dental Clinic</h2>
+            <img class="left" src="images/enerel-ped1.jpeg" alt="kid">
+            <img class="left" src="images/enerel-ped2.jpeg" alt="kid">
+            <img class="left" src="images/enerel-ped3.jpeg" alt="kid">
+            <p>There’s nothing more important than your child’s health and wellbeing. At "Enerel Dent" Dental Clinic, we care for your kids as if they were our own in our dedicated and separate kid-friendly zone. Our team always goes above and beyond to make sure every young patient’s visit is a positive experience that will set them up for a lifetime of optimal dental health.</p>
 
         </div>
-        <!--end row-->
+        <!--end text-->
+    </div>
+    <!--end row-->
 
+    <div class="text-2">
+        <h2>Learn more about "Enerel Dent"</h2>
+        <p>"Enerel Dent" Dental Clinic is providing a strict service to the anti-infection protection diet. Let's all prevent COVID-19 with collective force.
+            <br>🦷 Take an appointment on the phone or online through webpage first.
+            <br>🦷 Come to the hospital and show your tumblers.
+            <br>🦷 Answer the questionnaire about your current health condition.
+            <br>🦷 Sanitize your hands.
+            <br>🦷 If you need to wait in the waiting room, you will hold 6ft distance and sit in the hospital.
+            <br>🦷 When you enter the examination room, you will change your clothes and change your shoes and sanitize your hands again.
+            <br>🦷 Get the mask off with doctor's approval and rinse your mouth 1 minute with decontamination solution and throw it back in one time cup. The doctor will vacuum or pour it out from the cup.
+            <br>🦷 When you leave the treatment room, you will wear your mask back and sanitize your hands.
+            <br>🦷 Calculation of card or digital bank on reception.
+            Thank you. We hope you will support our work.</p>
+    </div>
 
-        <div class="row">
+</main>
 
-            <div class="text">
-                <h2> Pediatric Dental At "Enerel Dent" Dental Clinic</h2>
-                <img class="left" src="images/enerel-ped1.jpeg" alt="kid">
-                <p>There’s nothing more important than your child’s health and wellbeing. At "Enerel Dent" Dental Clinic, we care for your kids as if they were our own in our dedicated and separate kid-friendly zone. Our team always goes above and beyond to make sure every young patient’s visit is a positive experience that will set them up for a lifetime of optimal dental health.</p>
-
-            </div>
-            <!--end text-->
-        </div>
-        <!--end row-->
-
-        <div class="text-2">
-            <h2>Learn more about "Enerel Dent"</h2>
-            <p>No matter where you are in your dental health journey, "Enerel Dent" wants to help with guidance and support. By offering an affordable new patient appointment, we hope you'll call us today and become a patient for life!</p>
-        </div>
-
-    </main>
-
-    <aside>
-        <img src="images/dental-care.png" alt="teeth" width="100px">
-        <form action="formhandler1.php" method="post">
-            <div class="signup">
-                <h3>E-NEWSLETTER SIGNUP</h3>
-                <p>SUBSCRIBE</p>
-                <p>Subscribe to our newsletter for the latest news and services updates straight to your inbox.</p>
-            </div>
-            <div class="signup">
-                <input type="text" placeholder="Email" name="mail">
-                <input type="submit" value="Subscribe">
-
-            </div>
-        </form>
-    </aside>
-
-    <?php include 'includes/fp-footer.php' ?>
+<?php include 'includes/fp-footer.php' ?>
 
 </div> <!-- end wrapper -->
 </body>
